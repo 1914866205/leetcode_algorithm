bcc0ca460b6167b0f94930fd7b8051b9176cf267
nitaotao <1914866205@gmail.com>
2021-12-22 20:42:00 +0800

    题21

--- src/main/java/com/demo/course/hard/problem01/Solution.java
+++ src/main/java/com/demo/course/hard/problem01/Solution.java
@@ -1,65 +1,73 @@
 package com.demo.course.hard.problem01;
 
-import com.study.stream.Main;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 /**
  * author: nitaotao
- * date: 2021/12/21 19:16
+ * date: 2021/12/22 19:50
  * version: 1.0
  * description: https://leetcode-cn.com/problems/first-missing-positive/
  */
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {1,2,0};
-        System.out.println(solution.firstMissingPositive(arr));
+        int[] nums = {0, -1, 3, 1};
+        System.out.println(solution.firstMissingPositive(nums));
     }
 
     /**
-     * firstMissingPositive方法：找出其中没有出现的最小的正整数
+     * firstMissingPositive方法：找到缺失的第一个正数
      *
      * @param nums
      * @return
      */
     public int firstMissingPositive(int[] nums) {
-        //处理左边界
-        int min = nums[0];
+        //TreeSet 自动排序，去重
+        TreeSet treeSet = new TreeSet();
         int max = nums[0];
-        int sum = 0; //数组的和
-        boolean flag_0 = false;
+        int min = -1;
         for (int i = 0; i < nums.length; i++) {
-            //如果这个数比最小值还小，那么它就是最小值
-            if (min > nums[i]) {
+            if (nums[i] > 0) {
                 min = nums[i];
             }
-            //如果这个数比最大值还小，那么它就是最大值
+        }
+        //如果全是负数，返回1
+        if (min == -1) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
             if (max < nums[i]) {
                 max = nums[i];
             }
-            sum += nums[i];
+            if (nums[i] > 0 && min > nums[i]) {
+                min = nums[i];
+            }
+            treeSet.add(nums[i]);
         }
-        if (min > 1) {
+        //如果最大值小于1或者最小值大于1，返回1
+        if (max < 1 || min > 1) {
             return 1;
         }
-
-
-        //处理右边界，即左边为从0——n
-        //判断是不是顺序无差
-        System.out.println((min+max) * (max-min)*1.0 / 2);
-        if ((nums[0] + nums[nums.length - 1] * nums.length / 2 == sum)) {
-            if (max <= 0) {
-                return 1;
-            } else {
-                return max + 1;
+        Iterator iterator = treeSet.iterator();
+        int i = 1;
+        int num;
+        while (iterator.hasNext()) {
+            //遍历元素，当元素开始大于0时，开始比较。返回没出现的第一个正数
+            num = (int) iterator.next();
+            if (num > 0) {
+//                System.out.println(i + " " + num);
+                if (i != num) {
+                    return i;
+                }
+                i++;
+                if (i == max) {
+                    break;
+                }
             }
         }
-
-        //只处理正数
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-
-            }
-        }
-        return min;
+        //如果都出现了，就返回最大值+1
+        max++;
+        return max;
     }
 }
