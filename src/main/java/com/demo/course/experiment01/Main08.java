07f29af51d471d9e3aa7058e340f10d154b3d73b
nitaotao <1914866205@gmail.com>
2021-12-27 15:52:39 +0800

    实验一前7题

--- src/main/java/com/demo/course/experiment01/Main08.java
+++ src/main/java/com/demo/course/experiment01/Main08.java
@@ -23,31 +23,37 @@ public class Main08 {
      * @param c
      */
     public void factor(String c) {
+        TreeSet<Integer> set = new TreeSet();
         //最大位数
-        int[] a = new int[30];
+        int[] num = new int[30];
         String[] str = c.split("");
         //每一位转成数字
-        for(int i=0;i<str.length;i++) {
-            a[a.length-i-1] = Integer.parseInt(str[str.length-1-i]);
+        for (int i = 0; i < str.length; i++) {
+            num[num.length - i - 1] = Integer.parseInt(str[str.length - 1 - i]);
         }
-        int t;
-        int m = 0;
-        for(int i=2;i<=9;i++) {
-            t = 0;
-            for(int j=0;j<a.length;j++) {
-                if (a[j] + t >= i) {
-                    t = ((a[j] + t) % i) * 10;
-                } else {
-                    t = a[j] * 10;
-                }
+        int remainder; //余数
+        int tag = 0;  //记录有符合的大整数因子
+        for (int i = 2; i <= 9; i++) {
+            remainder = 0;
+            for (int j = 0; j <= num.length-1; j++) {
+                //模拟除法操作  除法操作从第一位开始进行，和大整数加法相反
+                //对余数进行运算
+                remainder = remainder * 10 + num[j];
+                remainder = remainder % i;   //余数
+
             }
-            if(t==0) {
-                System.out.print(i+" ");
-                m = 1;
+            if (remainder == 0) {  //当余数为0，说明可以整除
+                set.add(i);
+                tag = 1;
             }
         }
-        if (m == 0) {
+        if (tag == 0) {
             System.out.println("none");
+        } else {
+            Iterator<Integer> iterator = set.iterator();
+            while (iterator.hasNext()) {
+                System.out.print(iterator.next()+" ");
+            }
         }
     }
 }
